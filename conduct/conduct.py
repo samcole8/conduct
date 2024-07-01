6cff1f104eb1d3d5506a34af9f2a0a53663e11f3
Sam Cole <sam@samcole.me>
2024-07-01 18:14:05 +0100

    Import existing script

---
+++ conduct/conduct.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import sys
+import yaml
+import os
+import subprocess
+import shutil
+
+# Load CLI arguments
+secrets_path = sys.argv[1]
+docker_args = sys.argv[2:]
+
+# Get file locations
+codebase = os.path.abspath(".")
+work_dir = os.path.abspath(os.path.expanduser("~/.conduct"))
+
+# Load secrets into dict
+with open(secrets_path, "r") as secrets_file:
+    secrets = yaml.safe_load(secrets_file)
+
+# Clone directory with secrets
+for root, dirs, files in os.walk(codebase):
+    dest_root = root.replace(codebase, work_dir, 1)
+    if not os.path.exists(dest_root):
+        os.makedirs(dest_root)
+    for file in files:
+        source_file_path = os.path.join(root, file)
+        dest_file_path = os.path.join(dest_root, file)
+        with open(source_file_path, "r") as source_file, open(dest_file_path, "w") as dest_file:
+            for line in source_file:
+                flag_start = line.find("SECRET_")
+                if flag_start >= 0:
+                    flag_end = flag_start + 7
+                    secret_end = line.find('"', flag_end)
+                    key = line[flag_end:secret_end].lower()
+                    line = line[:flag_start] + secrets[key] + line[secret_end:]
+                dest_file.write(line)
+
+
+# Run docker compose on skeleton dir
+command = ['docker-compose', '--project-directory', f'"{work_dir}"', 'up'] + docker_args
+command = " ".join(command)
+process = subprocess.Popen(command, shell=True)
+process.wait()
+
+# Clean up
+shutil.rmtree(work_dir)
