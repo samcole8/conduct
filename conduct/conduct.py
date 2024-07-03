e7ce462092281772ea2e3afa897ec0f059f5b664
Sam Cole <sam@samcole.me>
2024-07-03 22:40:41 +0100

    Refactor script to use context manager
    - Encapsulated some code in a class to provide __entry__ and __exit__
      functions
    - Renamed some variables for clarity
    - Added some comments for clarity

--- conduct/conduct.py
+++ conduct/conduct.py
@@ -1,19 +1,68 @@
 #!/bin/python3
 
+import os
+import shutil
+import subprocess
 import sys
 import yaml
-import os
-import subprocess
-import shutil
 
-def load_args():
-    return sys.argv[1], sys.argv[2]
+FLAG = "SECRET_"
+
+class Fork:
+
+    def __init__(self, src, dst, secrets):
+        self.src = src
+        self.dst = dst
+        self.secrets = secrets
+
+    def __enter__(self):
+        def embed_secret(line):
+            # Get position of secret
+            flag_start = line.find(FLAG)
+            if flag_start != -1:
+                # Get secret start index
+                secret_start = flag_start + 7
+                secret_end = line.find('"', secret_start)
+                # Get secret end index
+                if secret_end == -1:
+                    if "\n" in line:
+                        secret_end = len(line) - 1
+                    else:
+                        secret_end = len(line)
+                # Get key from secret
+                key = line[secret_start:secret_end].upper()
+                # Construct new line
+                line = f"{line[:flag_start]}{self.secrets[key]}{line[secret_end:]}"
+            return line
+
+        # Fork directories
+        for src_root, dirs, files in os.walk(self.src):
+            dst_root = src_root.replace(self.src, self.dst, 1)
+            if not os.path.exists(dst_root):
+                os.makedirs(dst_root)
+            # Fork files
+            for file in files:
+                src_file_path = os.path.join(src_root, file)
+                dst_file_path = os.path.join(dst_root, file)
+                with open(src_file_path, "r") as src_file, open(dst_file_path, "w") as dst_file:
+                    # Embed secrets
+                    for line in src_file:
+                        dst_file.write(embed_secret(line))
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        shutil.rmtree(self.dst)
+    
+    def execute(self, command):
+        # Execute command
+        process = subprocess.Popen(command, shell=True, cwd=self.dst)
+        process.wait()
 
 def get_paths():
     # Get file locations
-    source = os.path.abspath(".")
-    work_dir = os.path.abspath(os.path.expanduser("~/.conduct"))
-    return source, work_dir
+    src = os.path.abspath(".")
+    dst = os.path.abspath(os.path.expanduser("~/.conduct"))
+    return src, dst
 
 def extract_secrets(secrets_file_path):
     # Load secrets into dict
@@ -21,46 +70,12 @@ def extract_secrets(secrets_file_path):
         secrets = yaml.safe_load(secrets_file)
     return secrets
 
-def sow(source, work_dir, secrets):
-    # Clone directory with secrets
-    for root, dirs, files in os.walk(source):
-        dest_root = root.replace(source, work_dir, 1)
-        if not os.path.exists(dest_root):
-            os.makedirs(dest_root)
-        for file in files:
-            source_file_path = os.path.join(root, file)
-            dest_file_path = os.path.join(dest_root, file)
-            with open(source_file_path, "r") as source_file, open(dest_file_path, "w") as dest_file:
-                for line in source_file:
-                    flag_start = line.find("SECRET_")
-                    if flag_start >= 0:
-                        flag_end = flag_start + 7
-                        secret_end = line.find('"' or " ", flag_end)
-                        print(secret_end)
-                        if secret_end == -1:
-                            if "\n" in line:
-                                secret_end = len(line) - 1
-                            else:
-                                secret_end = len(line)
-                        key = line[flag_end:secret_end].upper()
-                        line = line[:flag_start] + secrets[key] + line[secret_end:]
-                    dest_file.write(line)
-
-
-def execute(work_dir, command):
-    # Run docker compose on skeleton dir
-    process = subprocess.Popen(command, shell=True, cwd=work_dir)
-    process.wait()
-
-    # Clean up
-    shutil.rmtree(work_dir)
-
 def run():
-    secrets_file_path, command = load_args()
-    source, work_dir = get_paths()
-    secrets = extract_secrets(secrets_file_path)
-    sow(source, work_dir, secrets)
-    execute(work_dir, command)
+    secrets_path, command = sys.argv[1], sys.argv[2]
+    secrets = extract_secrets(secrets_path)
+    src, dst = get_paths()
+    with Fork(src, dst, secrets) as fork:
+        fork.execute(command)
 
 if __name__ == "__main__":
     run()
